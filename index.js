315e36db23242197f27f182fa9d168f3f0045dca
Danny Arnold <danny@dannyarnold.com>
2017-10-12 18:35:02 +0100

    sync from server

--- index.js
+++ index.js
@@ -1,20 +1,30 @@
-var app = require('express')();
-var server = require('http').Server(app);
-var io = require('socket.io')(server);
+var express = require('express');
+var https = require('https');
+var http = require('http');
+var fs = require('fs');
 
-server.listen(3000);
-app.get('/', function (req, res) {
-    res.send('ack2 "/" ');
 
+// This line is from the Node.js HTTPS documentation.
+var options = {
+  key: fs.readFileSync('/opt/bitnami/apache2/conf/server.key'),
+  cert: fs.readFileSync('nodecert.crt')
+};
+
+// Create a service (the app object is just a callback).
+var app = express();
+
+
+// Create an HTTPS service identical to the HTTP service.
+https.createServer(options, app).listen(3000);
+var io = require('socket.io')(https);
+
+app.get('/', function(req,res){
+
+        res.send("hi");
 });
 
-io.on('connection', function (socket) {
-    socket.emit('message', { hello: 'world' });
-    socket.on('message', function (data) {
-        console.log(data);
-    });
-
+io.on('connection',function (socket){
+		socket.emit('message', {hi:'there'});
+		socket.on('message',function (data){console.log(data); });
 });
-
-console.log('----------------- Listen');
 
